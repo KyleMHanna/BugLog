6c526e56f25cd43a805ff8f8b0dc8a688a443205
Kyle Hanna <Hanna.Kyle@live.com>
2021-10-02 16:14:44 -0600

    Working on bug detail card.

---
+++ BugLog.client/src/components/BugDetailCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="row">
+    <div class="col-12">
+      <div class="card">
+        <div class="card-body ">
+          <h5 class="card-title title-style">
+            {{ bug.title }}
+          </h5>
+          <div contenteditable="true">
+            <p class="card-text text-style">
+              {{ bug.description }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
